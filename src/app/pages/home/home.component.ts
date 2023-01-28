4208e65e0dc35595b2824ef3550ef388bcff7336
SAMAKE MOHAMED <39623776+youzerssif@users.noreply.github.com>
2023-01-28 00:15:54 +0000

    Add pie chart to home page succes

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { Observable, of } from 'rxjs';
 import { OlympicService } from 'src/app/core/services/olympic.service';
+import * as echarts from 'echarts';
+
 
 @Component({
   selector: 'app-home',
@@ -12,7 +14,50 @@ export class HomeComponent implements OnInit {
 
   constructor(private olympicService: OlympicService) {}
 
+  chartDom!:any;
+
   ngOnInit(): void {
     this.olympics$ = this.olympicService.getOlympics();
+    this.chartDom = document.getElementById('main');
+    let myChart = echarts.init(this.chartDom);
+    let option;
+
+    option = {
+      title: {
+        text: 'Referer of a Website',
+        subtext: 'Fake Data',
+        left: 'center'
+      },
+      tooltip: {
+        trigger: 'item'
+      },
+      legend: {
+        orient: 'vertical',
+        left: 'left'
+      },
+      series: [
+        {
+          name: 'Access From',
+          type: 'pie',
+          radius: '50%',
+          data: [
+            { value: 1048, name: 'Search Engine' },
+            { value: 735, name: 'Direct' },
+            { value: 580, name: 'Email' },
+            { value: 484, name: 'Union Ads' },
+            { value: 300, name: 'Video Ads' }
+          ],
+          emphasis: {
+            itemStyle: {
+              shadowBlur: 10,
+              shadowOffsetX: 0,
+              shadowColor: 'rgba(0, 0, 0, 0.5)'
+            }
+          }
+        }
+      ]
+    };
+
+    option && myChart.setOption(option);
   }
 }
