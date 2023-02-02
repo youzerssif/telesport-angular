fb162bfa7fd5c82d5dd3dac161b8352f2307140d
SAMAKE MOHAMED <39623776+youzerssif@users.noreply.github.com>
2023-02-02 23:28:36 +0000

    add detail graph for pie chart or country graph

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -15,12 +15,16 @@ export class HomeComponent implements OnInit {
   constructor(private olympicService: OlympicService) {}
 
   chartDom!:any;
+  chartDomDetail!:any;
 
   ngOnInit(): void {
     this.olympics$ = this.olympicService.getOlympics();
     this.chartDom = document.getElementById('main');
     let myChart = echarts.init(this.chartDom);
     let option;
+    this.chartDomDetail = document.getElementById('main-detail');
+    var myChartDetail = echarts.init(this.chartDomDetail);
+    var optionDetail;
 
     option = {
       title: {
@@ -41,11 +45,12 @@ export class HomeComponent implements OnInit {
           type: 'pie',
           radius: '50%',
           data: [
-            { value: 1048, name: 'Search Engine' },
-            { value: 735, name: 'Direct' },
-            { value: 580, name: 'Email' },
-            { value: 484, name: 'Union Ads' },
-            { value: 300, name: 'Video Ads' }
+            { value: 28, name: 'Italy' },
+            { value: 28, name: 'Germany' },
+            { value: 40, name: 'Spain' },
+            { value: 484, name: 'United State' },
+            { value: 484, name: 'United Kingdom' },
+            { value: 300, name: 'France' }
           ],
           emphasis: {
             itemStyle: {
@@ -57,6 +62,71 @@ export class HomeComponent implements OnInit {
         }
       ]
     };
+
+    optionDetail = {
+      title: {
+        text: 'Stacked Line'
+      },
+      tooltip: {
+        trigger: 'axis'
+      },
+      legend: {
+        data: ['Email', 'Union Ads', 'Video Ads', 'Direct', 'Search Engine']
+      },
+      grid: {
+        left: '3%',
+        right: '4%',
+        bottom: '3%',
+        containLabel: true
+      },
+      toolbox: {
+        feature: {
+          saveAsImage: {}
+        }
+      },
+      xAxis: {
+        type: 'category',
+        boundaryGap: false,
+        data: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun']
+      },
+      yAxis: {
+        type: 'value'
+      },
+      series: [
+        {
+          name: 'Email',
+          type: 'line',
+          stack: 'Total',
+          data: [120, 132, 101, 134, 90, 230, 210]
+        },
+        {
+          name: 'Union Ads',
+          type: 'line',
+          stack: 'Total',
+          data: [220, 182, 191, 234, 290, 330, 310]
+        },
+        {
+          name: 'Video Ads',
+          type: 'line',
+          stack: 'Total',
+          data: [150, 232, 201, 154, 190, 330, 410]
+        },
+        {
+          name: 'Direct',
+          type: 'line',
+          stack: 'Total',
+          data: [320, 332, 301, 334, 390, 330, 320]
+        },
+        {
+          name: 'Search Engine',
+          type: 'line',
+          stack: 'Total',
+          data: [820, 932, 901, 934, 1290, 1330, 1320]
+        }
+      ]
+    };
+    
+    optionDetail && myChartDetail.setOption(optionDetail);
 
     option && myChart.setOption(option);
   }
